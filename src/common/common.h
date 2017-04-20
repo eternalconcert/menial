25503377cfe9328f483720654c97b62618d724be
Christian <devnull@localhost>
2017-04-20 15:54:41 +0200

    Serving

--- src/common/common.h
+++ src/common/common.h
@@ -5,7 +5,7 @@
 
 const int BUFFER_SIZE = 256;
 const int BUFFER_LIMIT = BUFFER_SIZE - 1;
-const char END_OF_MESSAGE = EOF;
+const char END_OF_MESSAGE = '\n';
 
 void error(const char *msg);
 
--- src/server/messagehandler/http.cpp
+++ src/server/messagehandler/http.cpp
@@ -1,11 +1,19 @@
+#include <fstream>
 #include "http.h"
 
-std::string header = "HTTP/1.0 200 OK\nContent-Type: text/html; charset=utf-8\n\r";
+
+std::string readFromFile(std::string path) {
+    std::ifstream file(path);
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    return content;
+}
 
 std::string HttpHandler::handleMessage(std::string message) {
 
+	const std::string header = "HTTP/1.0 200 OK\nContent-Type: text/html; charset=utf-8\n\r\n";
+
     std::string reply = header;
-    reply += "Hallo Welt!";
+    reply += readFromFile("src/server/testdata/index.html");
 
     return reply;
 }
