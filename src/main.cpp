702280c9b7193cc46c1ef8ef0a3aa000884db696
Christian <devnull@localhost>
2017-05-02 23:07:55 +0200

    Started threading

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,29 @@
 #include "logger.h"
 #include "server.h"
 
+#include <thread>
 
 Logger* mainLogger = Logger::getLogger();
 static Config* config = Config::getConfig();
 
 
+void spanwServer(Server server) {
+    server.run();
+};
+
+
 int main(int argc, char *argv[]) {
     mainLogger->info("Starting menial");
     mainLogger->info("Initializing server");
-    Server server = Server(config->port);
-    mainLogger->info("Starting server");
-    server.run();
+    Server server1 = Server(8000);
+    Server server2 = Server(8080);
+
+    mainLogger->info("Starting servers");
+    std::thread thread1(spanwServer, server1);
+    std::thread thread2(spanwServer, server2);
+
+    thread1.join();
+    thread2.join();
 
     return 0;
 }
