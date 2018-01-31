f8bf37c1e480c91c0a840fb51dc0c7254bb83089
Christian <devnull@localhost>
2018-01-31 23:08:29 +0100

    Stuff

--- menial2/menial/src/main.rs
+++ menial2/menial/src/main.rs
@@ -1,10 +1,20 @@
+extern crate ansi_term;
+extern crate serde_json;
+
+use ansi_term::Colour::*;
+use serde_json::{Value, Error};
 use std::net::{TcpListener, TcpStream};
+use std::env;
+use std::process;
 use std::thread;
+
+
 
 
 struct Server {
     port: u16,
 }
+
 
 impl Server {
 
@@ -20,10 +30,23 @@ impl Server {
     }
 
 }
+
+fn parse_config(config_path: String) {
+    println!("{:?}", config_path);
+}
+
 fn main() {
 
-    let mut threads = vec![];
+    if env::args().len() < 2 {
+        println!("{}", Red.bold().paint("No config file path specified."));
+        process::exit(1);
+    }
 
+    let args: Vec<String> = env::args().collect();
+    let config_path = args[1].to_owned();
+    parse_config(config_path);
+
+    let mut threads = vec![];
     for i in 8080..8088 {
         let server: Server = Server { port: i };
         threads.push(
