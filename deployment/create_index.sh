dff67a965c9c4cb28caac7c56f7ea30f0d969c7a
Christian <devnull@localhost>
2018-04-27 23:46:57 +0200

    Stuff

--- deployment/create_index.sh
+++ deployment/create_index.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 OS_INFO=$(uname -mrs)
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
 cp deployment/index.neutral deployment/default/index.html
 sed -i -- 's/{{ OS_INFO }}/'"${OS_INFO}"'/g' deployment/default/index.html
+sed -i -- 's/{{ TIMESTAMP }}/'"${TIMESTAMP}"'/g' deployment/default/index.html
