418f6c66ba577584dd717a295672a3419baf156d
Christian <devnull@localhost>
2019-02-02 21:58:26 +0100

    Website more responsive

--- website/update_values.py
+++ website/update_values.py
@@ -5,9 +5,12 @@ from hashlib import sha256
 
 with open('build/menial.bin', 'r') as menial:
     menial_hash = sha256(menial.read()).hexdigest()
+    menial_hash = menial_hash[:32] + "<wbr>" + menial_hash[32:]
+
 
 with open('menial.tar.gz', 'r') as menial_tar_gz:
     menial_tar_gz_hash = sha256(menial_tar_gz.read()).hexdigest()
+    menial_tar_gz_hash = menial_tar_gz_hash[:32] + "<wbr>" + menial_tar_gz_hash[32:]
 
 with open('website/hashvalues.json', 'w+') as f:
     timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
