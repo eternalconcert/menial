74c15e5f5c0b88075ca6c60a9358ed3bbf30cb5b
Christian <devnull@localhost>
2019-04-24 16:23:50 +0200

    Stuff

--- testdata/testhost/wsgi_adapter.py
+++ testdata/testhost/wsgi_adapter.py
@@ -1,37 +1,47 @@
+from io import BytesIO
+import StringIO
 import sys
+
 sys.path.append("/home/xgwschk/projects/demobase/")
 sys.path.append("/home/xgwschk/projects/demobase/pythonenv/lib/python2.7/site-packages/")
 sys.path.append("/home/xgwschk/projects/menial/src/python/")
+
 from request import App, redirect, render, url_for, Request
 from base.wsgi import application as app
 
 
-result = []
+def make_headers(status, headers):
+
+    result = "HTTP/1.0 {status}\n" \
+             "Server: menial\n".format(status=status)
+
+    for key, value in headers:
+        result += key + ": " + value + "\n"
+
+    return result
 
 
-def get_response_headers(*args):
-
-    status = args[0]
-    headers = "HTTP/1.0 {status}\n" \
-              "Server: menial\n".format(status=status)
-
-    for key, value in args[1]:
-        headers += key + ": " + value + "\n"
-
-    result.append(headers)
 
 
-def dummy(*args):
-    pass
+def call_application(app, environ):
+    global status, headers
+    body = BytesIO()
 
+    def start_response(rstatus, rheaders):
+       global status, headers
+       status, headers = rstatus, rheaders
 
-#try:
-#    content = application(environ, get_response_headers).content.decode("utf-8")
-#    print(content)
-#except AttributeError:
-#    content = application(environ, dummy).streaming_content
-#    for item in content:
-#        print(item.decode("utf-8"))
+    app_iter = app(environ, start_response)
+    try:
+        for data in app_iter:
+            assert status is not None and headers is not None, "start_response was not called"
+            body.write(data)
+    except Exception as e:
+        pass
+    finally:
+        if hasattr(app_iter, 'close'):
+            app_iter.close()
+    return status, headers, body.getvalue()
 
 
 def application(*args):
@@ -41,8 +51,9 @@ def application(*args):
     header = args[2]
     body = args[3]
     request = Request(host, port, target, header, body)
-
-
+    wsgi_input = StringIO.StringIO()
+    wsgi_input.write(request.body)
+    wsgi_input.close()
     environ = {
         "REQUEST_METHOD": request.method,
         "SCRIPT_NAME": "",
@@ -51,12 +62,12 @@ def application(*args):
         "SERVER_NAME": request.host,
         "SERVER_PORT": request.port,
         "SERVER_PROTOCOL": "HTTP/1.0",
-        "wsgi.input": None,
+        "wsgi.input": wsgi_input,
         "wsgi.url_scheme": "",
         "wsgi.errors": ""
     }
-    #content = app(environ, get_response_headers)
-    content = app(environ, get_response_headers).content
-    result.append(content)
 
-    return "{0}\n{1}".format(result[0], result[1])
+    _status, _headers, _body = call_application(app, environ)
+    headers = make_headers(_status, _headers)
+
+    return "{0}\n{1}".format(headers, _body)
